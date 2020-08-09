6ce87a5c889202449ce7c31876691a827e715bd8
Yat Sun Lam <yatsunlam25@gmail.com>
2020-08-09 05:33:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
  def display_board
-   puts "A 3x3 Tic Tac Toe Board"
+   puts "a 3x3 tic tac toe board"
    puts "   |   |   "
    puts "-----------"
    puts "   |   |   "
